b9428937e9fc571b357e7c21f25ed42d7a495e89
Ashishpatel0512 <anpatel0512@gmail.com>
2025-12-28 21:24:25 +0530

    add

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
       <h1>🚀 Netlify CI/CD Demo</h1>
       <p>Version 1.0</p>
       <p>This is a simple demo application to showcase CI/CD with Netlify.</p>
+       <h2>Features:</h2>
     </div>
   );
 }
