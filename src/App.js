7674b364f26569e7c76a3ae7737ff9557d19646b
Ashishpatel0512 <anpatel0512@gmail.com>
2025-12-28 21:20:44 +0530

    add

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ function App() {
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>🚀 Netlify CI/CD Demo</h1>
       <p>Version 1.0</p>
+      <p>This is a simple demo application to showcase CI/CD with Netlify.</p>
     </div>
   );
 }
